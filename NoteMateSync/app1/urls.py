7e62963298551282c3339039f6cd82bbbb4f03a4
deepak-raaaz <deepakjamui26@gmail.com>
2023-09-09 01:21:51 +0530

    file change

---
+++ NoteMateSync/app1/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from app1 import views
+
+urlpatterns = [
+   path('',views.home,name='home'),
+   path('notes/',views.notes,name='notes'),
+   path('notes-view/',views.notesview,name='notesview'),
+   path('college/',views.college,name='college'),
+]
